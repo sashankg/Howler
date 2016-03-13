2a9c4b4656a411b41a57aa94d762a0ad3666d743
Sashank Gogula <sashankg@gmail.com>
2016-03-13 15:51:20 -0500

    Added Redux DevTools

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,10 +1,15 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import courses from '../reducers/courseReducer';
 
 export default function configureStore() {
     const reducers = combineReducers({
         courses
     });
-    const store = createStore(reducers);
+    const store = createStore(
+        reducers, 
+        compose(
+            window.devToolsExtension ? window.devToolsExtension() : f => f    
+        )
+    );
     return store;
 }
