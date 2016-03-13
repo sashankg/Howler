80d1be76c04d591f2776c8b67b8b445d2714f989
Sashank Gogula <sashankg@gmail.com>
2016-03-13 14:04:29 -0500

    Added EDIT_COURSE to courseReducer

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,22 +1,42 @@
+function Course(id, text="", dueDate=(new Date())) {
+    this.id = id;
+    this.text = text;
+    this.dueDate = dueDate;
+}
 export default function courseReducer(state = {array: [], entities: {}}, action) {
     switch(action.type) {
-        case 'ADD_COURSE': {
-            const id = new Date().getTime();
-            const array = state.array.concat(id);
-            return {
-                array,
-                entities: array.reduce((object, id) => {
-                    if(state.entities[id]) {
-                        object[id] = state.entities[id];
-                    }
-                    else {
-                        object[id] = new Course(id);
-                    }
-                    return object;
-                }, {})
-            }
+    case 'ADD_COURSE': {
+        const id = new Date().getTime();
+        const array = state.array.concat(id);
+        return {
+            array,
+            entities: array.reduce((object, id) => {
+                if(state.entities[id]) {
+                    object[id] = state.entities[id];
+                }
+                else {
+                    object[id] = new Course(id);
+                }
+                return object;
+            }, {})
         }
-        default: return state;
-    
+    }
+    case 'EDIT_COURSE': {
+        const array = state.array
+        return {
+            array,
+            entities: array.reduce((object, id) => {
+                if(id == action.id) {
+                    object[id] = action.data;
+                }
+                else {
+                    object[id] = state.entities[id];
+                }
+                return object;
+            })
+        }
+    }
+    default: return state;
+
     }
 }
