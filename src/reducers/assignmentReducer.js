61a493acf05b0c9bc5fdf05ba406bc19994681b2
Sashank Gogula <sashankg@gmail.com>
2016-03-13 17:38:46 -0500

    Added assignmentReducer

---
+++ src/reducers/assignmentReducer.js
@@ -0,0 +1,52 @@
+function Assignment(id, text="", dueDate=new Date(), completed=false) {
+    this.id = id;
+    this.text = text;
+    this.dueDate = dueDate;    
+}
+export default function assignmentReducer(state={ array: [], entities: {} }, action) {
+    switch(action.type) {
+    case 'ADD_ASSIGNMENT': {
+        const id = new Date().getTime();
+        const array = state.array.concat(id);
+        return {
+            array,
+            entities: array.reduce((object, id) => {
+                if(state.entities[id]) {
+                    object[id] = state.entities[id];
+                }
+                else {
+                    object[id] = new Assignment(id);
+                }
+                return object;
+            }, {})
+        }
+    } 
+    case 'EDIT_ASSIGNMENT': {
+        const array = state.array
+        return {
+            array,
+            entities: array.reduce((object, id) => {
+                if(id == action.id) {
+                    object[id] = action.data;
+                }
+                else {
+                    object[id] = state.entities[id];
+                }
+                return object;
+            })
+        }
+    }
+    case 'DELETE_ASSIGNMENT': {
+        const index = state.array.indexOf(action.id);
+        const array = state.array.slice(0, index).append(state.array.slice(index+1));
+        return {
+            array,
+            entities: array.reduce((object, id) => {
+                object[id] = state.entities[id];
+                return object;
+            }, {})
+        }
+    }
+    default: return state;
+    }
+}
